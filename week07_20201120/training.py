f7760264f00bb6fb9b434184b0d74969d1b20d6d
zhangjie49 <zhangjie49@baidu.com>
2020-11-24 00:10:15 +0800

    add evaluate model

--- week07_20201120/training.py
+++ week07_20201120/training.py
@@ -9,7 +9,7 @@ from bert4keras.snippets import open
 from keras.layers import Layer, Dense, Permute
 from keras.models import Model
 from tqdm import tqdm
-from evaluate import Evaluator
+from data.evaluate import Evaluator
 from keras.models import load_model
 
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -150,24 +150,63 @@ def main():
         os.path.join(data_dir,'demo_train.json')
     )
 
-    tokenizer = Tokenizer(dict_path, do_lower_case=True)
-
     model = build_transformer_model(
-        config_path,
-        checkpoint_path,
+    config_path,
+    checkpoint_path,
     )
 
     output = Dense(2)(model.output)
     output = MaskedSoftmax()(output)
     output = Permute((2, 1))(output)
 
+    from data.evaluate import evaluate as src_evaluate
+    from collections import OrderedDict
+    from keras.models import load_model
+
+    def evaluate(filename):
+        """
+        评测函数（官方提供评测脚本evaluate.py）
+        """
+        predict_to_file(filename, filename + '.pred.json')
+        ref_ans = json.load(io.open(filename))
+        pred_ans = json.load(io.open(filename + '.pred.json'))
+        F1, EM, TOTAL, SKIP = src_evaluate(ref_ans, pred_ans)
+        output_result = OrderedDict()
+        output_result['F1'] = '%.3f' % F1
+        output_result['EM'] = '%.3f' % EM
+        output_result['TOTAL'] = TOTAL
+        output_result['SKIP'] = SKIP
+        return output_result
+
+    class Evaluator(keras.callbacks.Callback):
+        """
+        评估和保存模型
+        """
+        def __init__(self):
+            self.best_val_f1 = 0.
+
+        def on_epoch_end(self, epoch, logs=None):
+            metrics = evaluate(
+                os.path.join(data_dir,'dev.json')
+                # os.path.join(data_dir,'demo_dev.json')
+            )
+            if float(metrics['F1']) >= self.best_val_f1:
+                self.best_val_f1 = float(metrics['F1'])
+                model.save_weights(os.path.join(output_dir,'roberta_best_model.weights'))
+                model.save(os.path.join(output_dir,'roberta_best_model.h5'))
+            metrics['BEST_F1'] = self.best_val_f1
+            print(metrics)
+
+    tokenizer = Tokenizer(dict_path, do_lower_case=True)
+
     model = Model(model.input, output)
     model.summary()
 
     model.compile(
-    loss=sparse_categorical_crossentropy,
-    optimizer=Adam(learing_rate),
-    metrics=[sparse_accuracy]
+        loss=sparse_categorical_crossentropy,
+        optimizer=Adam(learing_rate),
+        metrics=[sparse_accuracy]
+    )
 
     train_generator = data_generator(train_data, batch_size)
     evaluator = Evaluator()
